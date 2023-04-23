36b66764c3a7e62963638288e98b7f15aad63d7d
bngure <brianwahinyangure@gmail.com>
2023-04-23 08:24:02 +0300

    Added keyframes animations

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import { Super } from './pages';
 
 import configs from './configs/configs';
 
+import './css/App/App.css';
 import './css/style/style.css';
 
 const App = () => {
   const {home, homeB, homeC, news, error} = configs.links;
   return (
-    <Router>
-      <Routes>
-        <Route path={home} element={<Super entity="home"/>} />
-        <Route path={homeB} element={<Super entity="home"/>} />
-        <Route path={homeC} element={<Super entity="home"/>} />
-        <Route path={news} element={<Super entity="news"/>} />
-        <Route path={error} element={<Super entity="error"/>} />
-      </Routes>
-    </Router>
+    <section className="app">
+      <Router>
+        <Routes>
+          <Route path={home} element={<Super entity="home"/>} />
+          <Route path={homeB} element={<Super entity="home"/>} />
+          <Route path={homeC} element={<Super entity="home"/>} />
+          <Route path={news} element={<Super entity="news"/>} />
+          <Route path={error} element={<Super entity="error"/>} />
+        </Routes>
+      </Router>
+    </section>
   );
 }
 
